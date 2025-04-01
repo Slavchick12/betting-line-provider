47bdd817a60d0e9801039fbda816b93ed6c33a46
ocm <v.turukin@ocm-expert.com>
2025-04-01 10:17:18 +0300

    feat: typing for get_http_client

--- app/api/http_client.py
+++ app/api/http_client.py
@@ -1,10 +1,11 @@
 from contextlib import asynccontextmanager
+from typing import AsyncGenerator
 
 from httpx import AsyncClient
 
 
 @asynccontextmanager
-async def get_http_client() -> AsyncClient:
+async def get_http_client() -> AsyncGenerator[AsyncClient, None]:
     async with AsyncClient() as client:
         try:
             yield client
